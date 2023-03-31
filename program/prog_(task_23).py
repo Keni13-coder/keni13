cd1ad35e37aa800be8490d69e5ecc955d86b92fa
Vlad <vlad.moldovanov.2014@mail.ru>
2023-03-31 10:53:07 +0300

    added task 23

---
+++ program/prog_(task_23).py
@@ -0,0 +1,5 @@
+def find_needle(haystack):
+    return f"found the needle at position {haystack.index('needle')}"
+
+
+print(find_needle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']))
