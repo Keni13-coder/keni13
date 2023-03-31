ea8e20e736bcf488de346880a8a2e95d5db85124
Vlad <vlad.moldovanov.2014@mail.ru>
2023-03-31 16:05:18 +0300

    added task 27 ***

---
+++ program/prog_(task_27).py
@@ -0,0 +1,18 @@
+from functools import reduce
+def nb_year(p0, percent, aug, p,):
+    # count=0
+    # while p0 < p: p0+=p0 * percent/100 + aug ;count+=1
+    # if p0 >= p:
+    #     return count            
+    # return count
+     
+    # return i if p0>=p else nb_year(int(p0+p0*(percent/100)+aug), percent, aug, p, i+1)
+
+    return (0 if p0 >= p else 1 + nb_year(int(p0*(1+percent*.01)+aug), percent, aug, p))
+
+
+
+print(nb_year(1500, 5, 100, 5000))
+
+
+# eval(f'{aug}+({p0}*{percent}//100)+{p0}')
