94dbd166c574fd40f81ba501bfba32f5d963e863
Vlad <vlad.moldovanov.2014@mail.ru>
2023-04-18 16:50:01 +0300

    +

--- program/prog_(task_36).py
+++ program/prog_(task_36).py
@@ -1,15 +1,16 @@
 from functools import reduce
 def snail(array):
-    # return list(filter(lambda x: x.isdigit(), map(lambda x: sorted(str(x), key=max) ,array)))
-    # return list(map(str,map(lambda z:reduce(lambda x,y: x and y,z),array)))
-    new_array =[]
-    return  " ".join([" ".join([str(i) for i in x if str(i).isdigit()]) for x in array]).split()
+    # return  list(map(int,sorted(" ".join([" ".join([str(i) for i in x if str(i).isdigit()]) for x in array]).split())))
+    return  ([([i for i in x if str(i).isdigit()]) for x in array])[1][::-1].extend()
 
 
 
+# array = [[1,2,3],
+#         [9,5,6],
+#         [7,8,9]]
 array = [[1,2,3],
-        [4,5,6],
-        [7,8,9]]
-
+          [4,5,6],
+          [7,8,9]]
+# expected = [1,2,3,6,9,8,7,4,5]
 
 print(snail(array))
