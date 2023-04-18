babdc412c6d86f45a197ed259d5512399e41e4d5
vlad <vlad.moldovanov.2014@mail.ru>
2023-04-18 21:04:57 +0300

    added Task_36

--- program/prog_(task_36).py
+++ program/prog_(task_36).py
@@ -1,9 +1,20 @@
-from functools import reduce
+
 def snail(array):
     # return  list(map(int,sorted(" ".join([" ".join([str(i) for i in x if str(i).isdigit()]) for x in array]).split())))
-    return  ([([i for i in x if str(i).isdigit()]) for x in array])[1][::-1].extend()
+    my_list = [([i for i in x if str(i).isdigit()]) for x in array]
+    firs_list = my_list[1][::-1]
+    # firs_list.insert(1,[1,2,3,4])
+    # my_list[2][::-1]
+    # firs_list.insert(0,my_list[0])
+    firs_list.insert(1,my_list[2][::-1])
+    # firs_list[1] = int(lambda x: int(x))
 
 
+    return list(map(int," ".join([str(x) if isinstance(x,int) else " ".join([str(i) for i in x]) for x in eval(f'{my_list[0]}+{firs_list}')]).split()))
+
+
+# f'{my_list[0]}{ firs_list}'
+# eval(f'{my_list[0]}+{firs_list}')
 
 # array = [[1,2,3],
 #         [9,5,6],
