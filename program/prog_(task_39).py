90bb85d5459820a0780ddd483a7cabea1b9a4585
Vlad <vlad.moldovanov.2014@mail.ru>
2023-04-20 16:00:42 +0300

    added Task-39

---
+++ program/prog_(task_39).py
@@ -0,0 +1,17 @@
+def series_sum(n):
+    i = 1
+    z = 4
+    suma = 1
+    if n > 1:
+        while i < n:
+            suma += 1/z
+            z += 3
+            i += 1
+    else:
+        return f'{n}.00'       
+    return "{:.2f}".format(suma)
+# [x for x in range(1,n)]
+print(series_sum(59))
+
+def series_sum(n):
+    return '{:.2f}'.format(sum(1.0/(3 * i + 1) for i in range(n)))
