c4339eb34aa23feeeaaa57c3fbfcc4e5d1ac0c75
Vlad <vlad.moldovanov.2014@mail.ru>
2023-04-21 09:46:50 +0300

    f

--- program/prog_(task_39).py
+++ program/prog_(task_39).py
@@ -13,5 +13,8 @@ def series_sum(n):
 # [x for x in range(1,n)]
 print(series_sum(59))
 
-def series_sum(n):
-    return '{:.2f}'.format(sum(1.0/(3 * i + 1) for i in range(n)))
+def series_sums(n):
+    # return '{:.2f}'.format(sum(1.0/(3 * i + 1) for i in range(n)))
+    return f"{sum(1 / i for i in range(1, (n * 3) + 1, 3)): .2f}"
+
+print(series_sums(59))
