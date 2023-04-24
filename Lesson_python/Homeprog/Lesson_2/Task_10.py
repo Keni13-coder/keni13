da685558cdedc88cd1a623b8b2eaf4d6a4073282
Vlad <vlad.moldovanov.2014@mail.ru>
2023-04-24 15:25:36 +0300

    update task_10

--- Lesson_python/Homeprog/Lesson_2/Task_10.py
+++ Lesson_python/Homeprog/Lesson_2/Task_10.py
@@ -5,9 +5,10 @@
 # Честно я не совсем понял задание. Откуда берётся информация о расположении монет.
 # Если ифнформацию о монетах превратить в массив, то получаться так:
 
-def task10(array_money):
-    return array_money.count(0) if array_money.count(1) > array_money.count(0) else array_money.count(1)
+def task10(N,array_money):
+    # return array_money.count(0) if array_money.count(1) > array_money.count(0) else array_money.count(1)
+    return min(sum(array_money),N-sum(array_money))
 
 
 
-print(task10([1,0,1,1,0]))
+print(task10(5,[1,0,0,0,0]))
