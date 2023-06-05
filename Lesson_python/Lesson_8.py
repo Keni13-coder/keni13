39fd56d973af4b8742c78a9e9a011008f825aa08
Vlad <vlad.moldovanov.2014@mail.ru>
2023-06-05 15:59:21 +0300

    up less_8

--- Lesson_python/Lesson_8.py
+++ Lesson_python/Lesson_8.py
@@ -42,9 +42,9 @@ try:
 name = None
 def start():
     global name
-    name = input('Введите имя файла для работы справочника: ')
+    name = input('Введите имя текстового файла  для работы справочника или exit для выхода: ')
     if name == 'exit':
-        pass
+        return "exit"
     return name
 def show_menu():
     print("\nВыберите необходимое действие:\n"
@@ -53,11 +53,13 @@ def show_menu():
           "2. Найти абонента по фамилии\n"
           "3. Найти абонента по номеру телефона\n"
           "4. Добавить абонента в справочник\n"
-          "5. Клонировать справочник в текстовом формате\n"
-          "6. Закончить работу")
+          "5. Сохранить справочник в текстовом формате\n"
+          "6. Удалить абонента\n"
+          "7. Изменение данных\n"
+          "8. Завершить работу\n")
     choice = int(input('Введите номер действия: ').strip())
 
-    while choice > 6:
+    while choice > 8:
         print('Введенный номер не соответствует командам!!!')
         print("\nВыберите необходимое действие:\n"
           "0. Создать справочник\n"
@@ -67,7 +69,9 @@ def show_menu():
           "4. Добавить абонента в справочник\n"
           "5. Сохранить справочник в текстовом формате\n"
           "6. Удалить абонента\n"
-          "7. Изменение данных\n")
+          "7. Изменение данных\n"
+          "8. Завершить работу\n")
+        
         choice = int(input('Введите номер действия: ').strip())
     return choice
 
@@ -85,15 +89,14 @@ def funk_1():
         [print(x,end='') for x in n ]
 
 
-# funk_1('file.txt')
-# show_menu()
+
 def funk_2():
     cursor = input('Введите фамилию: ').strip()
     with open(f'{name}.txt','r+',encoding='utf=8') as f:
         [ print(x[:-1]) if  '\n' in x else print(x) for x in f if cursor.casefold() in x.casefold()]
 
     
-# funk_2('file.txt')
+
 
 def funk_3():
     cursor = input('Введите телефон: ').strip()
@@ -101,7 +104,7 @@ def funk_3():
         [ print(x[:-1]) if  '\n' in x else print(x) for x in f if cursor.casefold() in x.casefold()]
 
 
-# funk_3('file.txt')
+
 
 
 def funk_4():
@@ -116,7 +119,7 @@ def funk_4():
         print('Запись добавлена')
         
 def funk_5():
-    second_name = input('Введите имя файла клона: ')
+    second_name = input('Введите имя файла клона: ').strip()
     with open(f'{second_name}.txt','w+',encoding='utf=8') as f: 
        return shutil.copy(f'{name}.txt',f'{second_name}.txt')
     
@@ -126,13 +129,83 @@ def funk_6():
     two_flag = input('Введите фамилию абонента: ').strip()
     with open(f'{name}.txt','r+',encoding='utf=8') as f:
        d = f.readlines()
-    with open(f'{name}.txt','w+',encoding='utf=8') as f:   
-        for number,line in enumerate(d):
-            if one_flag not in line and two_flag not in line:
+    with open(f'{name}.txt','r+',encoding='utf=8') as f:
+  
+        for number,line in enumerate(d,1):
+            if one_flag in line and two_flag in line:
+                print(f'{number}: {line}',end='') if number != len(d) else print(f'{number}: {line}',end='\n')
+    delete = int(input("Введите норме записи для удаление: ").strip())
+    with open(f'{name}.txt','w+',encoding='utf=8') as f:
+        for number,line in enumerate(d,1):            
+            if number != delete:
                 f.write(line)
+                print('Запись была удалена')
 
 
 
+def funk_7():
+    print('Выберите номер желаемого изменения\n'
+          '1. Изменить Имя\n'
+          '2. Изменить Фамилию\n'
+          '3. Изменитиь Отчество\n'
+          '4. Изменить Номер телефона\n')
+    
+    flag = int(input('Введите номер изменения: ').strip())
+    if flag == 1:
+        first_names = input('Введите имя которое хотели бы поменять: ').strip()
+        second_names = input('Введите имя на которое хотели бы поменять: ').strip()
+    elif flag == 2:
+        first_names = input('Введите фамилию которую хотели бы поменять: ').strip()
+        second_names = input('Введите фамилию на которую хотели бы поменять: ').strip()
+    elif flag == 3:
+        first_names = input('Введите отчество которое хотели бы поменять: ').strip()
+        second_names = input('Введите отчество на которое хотели бы поменять: ').strip()    
+    elif flag == 4:
+        first_names = input('Введите номер телефона который хотели бы поменять: ').strip()
+        second_names = input('Введите номер телефона на который хотели бы поменять: ').strip()    
+    while flag > 4:
+        print('Введённый номер не существует')
+        print('Выберите номер желаемого изменения\n'
+        '1. Изменить Имя\n'
+        '2. Изменить Фамилию\n'
+        '3. Изменитиь Отчество\n'
+        '4. Изменить Номер телефона\n')
+        if flag == 1:
+            first_names = input('Введите имя которое хотели бы поменять: ').strip()
+            second_names = input('Введите имя на которое хотели бы поменять: ').strip()
+        elif flag == 2:
+            first_names = input('Введите фамилию которую хотели бы поменять: ').strip()
+            second_names = input('Введите фамилию на которую хотели бы поменять: ').strip()
+        elif flag == 3:
+            first_names = input('Введите отчество которое хотели бы поменять: ').strip()
+            second_names = input('Введите отчество на которое хотели бы поменять: ').strip()    
+        elif flag == 4:
+            first_names = input('Введите номер телефона который хотели бы поменять: ').strip()
+            second_names = input('Введите номер телефона на который хотели бы поменять: ').strip()
+            
+                                            
+    with open(f'{name}.txt','r+',encoding='utf=8') as f:
+        d = f.readlines()
+    with open(f'{name}.txt','r+',encoding='utf=8') as f:
+        for numbers ,search in enumerate(d,1):
+            if first_names in search:
+                print(f'{numbers}: {search}',end='') if numbers != len(d) else print(f'{numbers}: {search}',end='\n')
+        num = int(input("Введите номер позиции заменяемой строки: "))
+        while num > 4:
+            print('Введенный номер не существует!!!')
+            num = int(input("Введите номер позиции заменяемой строки: "))
+    with open(f'{name}.txt','w+',encoding='utf=8') as f:
+        for numbers ,search in enumerate(d,1):             
+            if numbers != num:
+                f.write(f'{search}')
+            else:
+                if numbers == num:
+                    new_search = search.replace(first_names, second_names)
+                    f.write(f'{new_search}')
+                    print(f"в строке {numbers} заменено {first_names} на {second_names}")
+                    
+                        
+                
 def all_functional(int):
     if int == 0:
         return zero()
@@ -146,27 +219,32 @@ def all_functional(int):
         return funk_4()
     elif int == 5 :
         return funk_5()
+    elif int == 6 :
+        return funk_6()
+    elif int == 7 :
+        return funk_7()
 
 
 def main():
     print('Приветствую вас в программе "Так себе код"')
-    start()
-    sh = 0
-    while sh < 8:
-        sh = show_menu()
-        all_functional(sh)
+    s = start()
+    if s == 'exit':
+        print('Завершение программы.')
+    else:    
+        sh = 0
+        while sh < 8:
+            sh = show_menu()
+            all_functional(sh)
 
-    print('Завершение программы.')
+        print('Завершение программы.')
 
 
 
-# try:
-#     main()
+try:
+    main()
 
-# except Exception as ex:
-#     while ex:
-#         print(f'{ex}')
-#         main()        
-start()
-# zero()
-funk_6()
+except Exception as ex:
+    while ex:
+        print(f'{ex}')
+        main()        
+
