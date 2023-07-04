26b7d84ab27718afe8ee407a5e792add29752bd7
Vlad <vlad.moldovanov.2014@mail.ru>
2023-07-04 15:20:59 +0300

    04.07.2023

--- Lesson_python/lesson_second/lesson/lesson_6.py
+++ Lesson_python/lesson_second/lesson/lesson_6.py
@@ -3,3 +3,19 @@
 'создание пакетов производиться с помощью файла __init__ можно и пустого, пакет может быть вложенным'
 'пакет состоит из папки и файлом __init__ в нутри, так же там есть модули и возможно другие пакеты'
 'sys argv - для получение списка элементов при вводе в командной строке через консоль'
+
+from modules.find import find_number as find
+import modules.secret as secr
+from sys import argv as ar
+import modules.calendar as cal
+
+
+def main():
+    arguments = ar[1:]
+    funk = find(*map(int,arguments))
+    if funk:
+        return "все класс"
+    return "хуила"
+# secr.dictionary_secret()
+# print(secr.rezul_dict())
+print(cal.date('29.02.2001'))
